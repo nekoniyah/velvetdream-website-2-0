028107fc437d5a74e6b35552a0a8511d998a5bc9
nekomancer0 <nekomancers0@gmail.com>
2025-01-04 00:05:42 +0100

    t

--- api/server.js
+++ api/server.js
@@ -52,7 +52,7 @@ app.post("/api/mail", async (req, res) => {
 app.use(express.static(path.join(__dirname, "..", "dist")));
 
 // Handle SPA routing - send all requests to index.html
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "..", "index.html"));
 });
 
