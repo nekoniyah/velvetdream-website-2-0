540e8a88e8bc2571a671f955968048a26bfda99f
nekomancer0 <nekomancers0@gmail.com>
2025-02-05 20:18:03 +0100

    aaa

--- src/pages/Store.svelte
+++ src/pages/Store.svelte
@@ -9,13 +9,7 @@
       Check out our official merchandise! Show your support with our custom designs.
     </p>
     <div class="store-embed">
-      <iframe
-        src="https://fluffworks.creator-spring.com"
-        title="VelvetDream Merchandise Store"
-        width="100%"
-        height="800px"
-        frameborder="0"
-      />
+    <iframe style="border-radius:24px;border:none" src="https://embed.creator-spring.com/widget?slug=my-store-104f153&per=20&currency=&page=1&layout=carousel-small&theme=light" title="my-store-104f153 Merch store powered by Spring" width="100%" height="420" data-reactroot=""></iframe>
     </div>
   </div>
 </div>
