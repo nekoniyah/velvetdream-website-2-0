5cdc0135fe8d15acea349c563b31f5e74816ca79
nekoniyah <beastish.maki@gmail.com>
2025-02-10 23:12:18 +0100

    e

--- api/server.js
+++ api/server.js
@@ -24,10 +24,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "..", "dist")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
 const router = express.Router();
